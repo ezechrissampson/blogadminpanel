35db47eecd1e6abc066e76002a626a928d59efbc
ezechrissampson <ezechrissampson@gmail.com>
2025-10-28 17:53:07 +0100

    Sidebar and Dashboard

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import "./sidebar.css"
+
+const Sidebar = () => {
+
+
+    return(
+        <>
+        
+        <div className="sidebar d-flex flex-column">
+    <h5><i className="bi"></i>Ezechrissampson</h5>
+    <ul className="nav flex-column">
+
+      <li className="nav-item position-relative">
+        <a className="nav-link active" href="#"><i className="bi bi-speedometer2"></i> Dashboard</a>
+        <div className="submenu">
+          <a href="#"><i className="bi bi-house"></i> Home</a>
+          <a href="#"><i className="bi bi-arrow-repeat"></i> Updates</a>
+        </div>
+      </li>
+
+      <li className="nav-item position-relative">
+        <a className="nav-link" href="#"><i className="bi bi-stickies"></i> Posts</a>
+        <div className="submenu">
+          <a href="#"><i className="bi bi-list-ul"></i> All Posts</a>
+          <a href="#"><i className="bi bi-plus-lg"></i> Add New</a>
+          <a href="#"><i className="bi bi-tags"></i> Categories</a>
+        </div>
+      </li>
+
+      <li className="nav-item position-relative">
+        <a className="nav-link" href="#"><i class="bi bi-image"></i> Media</a>
+        <div className="submenu">
+          <a href="#"><i className="bi bi-images"></i> Library</a>
+          <a href="#"><i className="bi bi-upload"></i> Add New</a>
+        </div>
+      </li>
+
+      <li className="nav-item position-relative">
+        <a className="nav-link" href="#"><i className="bi bi-file-earmark-text"></i> Pages</a>
+        <div className="submenu">
+          <a href="#"><i className="bi bi-collection"></i> All Pages</a>
+          <a href="#"><i className="bi bi-plus-square"></i> Add New</a>
+        </div>
+      </li>
+
+      <li className="nav-item position-relative">
+        <a className="nav-link" href="#"><i className="bi bi-chat-dots"></i> Comments</a>
+      </li>
+
+      <li className="nav-item position-relative">
+        <a className="nav-link" href="#"><i className="bi bi-palette"></i> Appearance</a>
+        <div className="submenu">
+          <a href="#"><i className="bi bi-brush"></i> Themes</a>
+          <a href="#"><i className="bi bi-tools"></i> Customize</a>
+        </div>
+      </li>
+
+      <li className="nav-item position-relative">
+        <a className="nav-link" href="#"><i className="bi bi-plug"></i> Plugins</a>
+        <div className="submenu">
+          <a href="#"><i className="bi bi-puzzle"></i> Installed</a>
+          <a href="#"><i className="bi bi-plus-circle"></i> Add New</a>
+        </div>
+      </li>
+
+      <li className="nav-item position-relative">
+        <a className="nav-link" href="#"><i class="bi bi-people"></i> Users</a>
+        <div className="submenu">
+          <a href="#"><i className="bi bi-person-lines-fill"></i> All Users</a>
+          <a href="#"><i className="bi bi-person-plus"></i> Add New</a>
+        </div>
+      </li>
+
+      <li className="nav-item position-relative">
+        <a className="nav-link" href="#"><i className="bi bi-wrench"></i> Tools</a>
+        <div className="submenu">
+          <a href="#"><i className="bi bi-upload"></i> Import</a>
+          <a href="#"><i className="bi bi-download"></i> Export</a>
+        </div>
+      </li>
+
+      <li className="nav-item position-relative">
+        <a className="nav-link" href="#"><i className="bi bi-gear"></i> Settings</a>
+        <div className="submenu">
+          <a href="#"><i className="bi bi-sliders"></i> General</a>
+          <a href="#"><i className="bi bi-link-45deg"></i> Permalinks</a>
+        </div>
+      </li>
+
+    </ul>
+  </div>
+
+
+        </>
+    )
+}
+
+export default Sidebar
---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+
+const Dashboard = () =>{
+
+    return(
+    <>
+    <Sidebar />
+
+
+  <div className="main-content">
+    <h2>Dashboard</h2>
+    <p>Your dashboard content goes here...</p>
+  </div>
+    
+    </>
+    )
+}
+
+export default Dashboard
